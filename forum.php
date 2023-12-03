f02f99b785f8700a8b48dff561d25e577f88c8c3
userwei <chou.ting.wei@outlook.com>
2023-12-03 23:52:05 +0800

    Fix bug

--- forum.php
+++ forum.php
@@ -176,11 +176,31 @@
                 echo "<tbody>";
                 if($search != ""){
                     $forumData = getForumList($search);
-                    $forumCnt = count($forumData);
+                    $forumCnt = 0;
+                    if($forumData != NULL){
+                        $forumCnt = count($forumData);
+                    }
                     if($forumCnt > 0){
                         for($index = 0; $index < $forumCnt; $index ++){
-                            $forum = $forumData[$index];
-                            echo "";
+                            $forum = $forumData[$index]->get_all();
+                            echo "<tr>";
+                            echo "<td class='align-middle'>".$forum[0]."</td>";
+                            echo "<td class='align-middle'>".$forum[2]."</td>";
+                            echo "<td class='align-middle'>".$forum[3]."</td>";
+                            echo "<td class='align-middle'>";
+                            if(isset($_SESSION['username'])){
+                                $username = $_SESSION['username'];
+                                if($username == "admin" || $username == $forum[2]){
+                                    echo "<button class='btn btn-danger' type='button' onclick=\"_delForum('".$forum[0]."')\">";
+                                    echo "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>";
+                                    echo "<path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z'/>";
+                                    echo "<path d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z'/>";
+                                    echo "</svg>";
+                                    echo "</button>";
+                                }
+                            }
+                            echo "</td>";
+                            echo "</tr>";
                         }
                     }
                     else{
