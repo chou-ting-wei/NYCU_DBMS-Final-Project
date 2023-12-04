7cad71590817caa16f366c66f4dc6c6b8e15dde0
userwei <chou.ting.wei@outlook.com>
2023-12-05 00:15:19 +0800

    Fix vote

--- vote.php
+++ vote.php
@@ -206,20 +206,53 @@
                 echo "</tr></thead>";
                 echo "<tbody>";
                 if($search != ""){
-                    // $voteData = getVoteList($search);
-                    // $voteCnt = 0;
-                    // if($voteData != NULL){
-                    //     $voteCnt = count($voteData);
-                    // }
-                    // if($voteCnt > 0){
-                    //     for($index = 0; $index < $voteCnt; $index ++){
-                    //         $vote = $voteData[$index]->get_all();
-                            
-                    //     }
-                    // }
-                    // else{
+                    $voteData = getVoteList($search);
+                    $voteCnt = 0;
+                    if($voteData != NULL){
+                        $voteCnt = count($voteData);
+                    }
+                    if($voteCnt > 0){
+                        for($index = 0; $index < $voteCnt; $index ++){
+                            $vote = $voteData[$index]->get_all();
+                            echo "<tr>";
+                            echo "<td class='align-middle'><a href='#' data-bs-toggle='modal' data-bs-target='#voteModalIdx".$index."'>".$vote[0]."</a></td>";
+                            // echo "<td class='align-middle'>".$vote[0]."</td>";
+                            echo "<td class='align-middle text-center'>".$vote[1]."</td>";
+                            echo "<td class='align-middle'>";
+                            echo "<div class='progress'>";
+                            if($vote[1] + $vote[2] == 0){
+                                echo "<div class='progress-bar' role='progressbar' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100'></div>";
+                            }
+                            else{
+                                $tmp = round($vote[1] * 100 / ($vote[1] + $vote[2]));
+                                echo "<div class='progress-bar' role='progressbar' style='width: ".$tmp."%' aria-valuenow='".$tmp."' aria-valuemin='0' aria-valuemax='100'></div>";
+                                echo "<div class='progress-bar bg-danger' role='progressbar' style='width: ".(100 - $tmp)."%' aria-valuenow='".(100 - $tmp)."' aria-valuemin='0' aria-valuemax='100'></div>";
+                            }
+                            echo "</div>";
+                            echo "</td>";
+                            echo "<td class='align-middle text-center'>".$vote[2]."</td>";
+                            echo "<td class='align-middle'>".$vote[3]."</td>";
+                            echo "<td class='align-middle'>".$vote[3]."</td>";
+                            echo "<td class='align-middle'>";
+                            if(isset($_SESSION['username'])){
+                                $username = $_SESSION['username'];
+                                if($username == "admin"){
+                                    echo "<button class='btn btn-danger' type='button' onclick=\"_delVote('".$vote[0]."')\">";
+                                    echo "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>";
+                                    echo "<path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z'/>";
+                                    echo "<path d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z'/>";
+                                    echo "</svg>";
+                                    echo "</button>";
+                                }
+                            }
+                            echo "</td>";
+                            echo "</tr>";
+                        }
+                    }
+                    else{
                         echo "<tr><td class='align-middle' colspan='7'><span class='text-danger mb-3'>No result found.</span></td></tr>";
-                    // }
+                    }
+                    echo "</tbody></table></div></div>";
                 }
                 else{
                     $voteData = getVoteList("");
