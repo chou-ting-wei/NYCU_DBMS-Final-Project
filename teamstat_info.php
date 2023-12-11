a5f067209bfc1aafac3309de6d58fc3a01b0bcc0
userwei <chou.ting.wei@outlook.com>
2023-12-12 01:02:39 +0800

    Add teamstat

--- teamstat_info.php
+++ teamstat_info.php
@@ -6,9 +6,6 @@
         $_SESSION["login_session"] = false;
         $_SESSION["username"] = NULL;
     }
-    if(!isset($_SESSION["team_info"])){
-        $_SESSION["team_info"] = NULL;
-    }
     include("sqlmanager.php")
 ?>
 <html>
@@ -84,60 +81,24 @@
         <div class="container mt-4">
             <h3 class="fw-bolder">Team Stat</h3>
             <hr class="mt-3 mb-3"></hr>
-            <?php
-                $voteData = getVoteList("");
-                $voteCnt = 0;
-                if($voteData != NULL){
-                    $voteCnt = count($voteData);
-                }
-                if($voteCnt > 0){
-                    for($index = 0; $index < $voteCnt; $index ++){
-                        $vote = $voteData[$index]->get_all();
-                        // echo "<pre>";
-                        // print_r($vote);
-                        // echo "</pre>";
-                        echo "<tr>";
-                        echo "<td class='align-middle'><a href='#' data-bs-toggle='modal' data-bs-target='#voteModalIdx".$index."'>".$vote[0]."</a></td>";
-                        // echo "<td class='align-middle'>".$vote[0]."</td>";
-                        echo "<td class='align-middle text-center'>".$vote[1]."</td>";
-                        echo "<td class='align-middle'>";
-                        echo "<div class='progress'>";
-                        if($vote[1] + $vote[2] == 0){
-                            echo "<div class='progress-bar' role='progressbar' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100'></div>";
-                        }
-                        else{
-                            $tmp = round($vote[1] * 100 / ($vote[1] + $vote[2]));
-                            echo "<div class='progress-bar' role='progressbar' style='width: ".$tmp."%' aria-valuenow='".$tmp."' aria-valuemin='0' aria-valuemax='100'></div>";
-                            echo "<div class='progress-bar bg-danger' role='progressbar' style='width: ".(100 - $tmp)."%' aria-valuenow='".(100 - $tmp)."' aria-valuemin='0' aria-valuemax='100'></div>";
-                        }
-                        echo "</div>";
-                        echo "</td>";
-                        echo "<td class='align-middle text-center'>".$vote[2]."</td>";
-                        echo "<td class='align-middle'>".$vote[4]."</td>";
-                        echo "<td class='align-middle'>".$vote[3]."</td>";
-                        echo "<td class='align-middle'>";
-                        if(isset($_SESSION['username'])){
-                            $username = $_SESSION['username'];
-                            if($username == "admin" || $username = $vote[4]){
-                                echo "<button class='btn btn-danger' type='button' onclick=\"_delVote('".$vote[0]."')\">";
-                                echo "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>";
-                                echo "<path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z'/>";
-                                echo "<path d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z'/>";
-                                echo "</svg>";
-                                echo "</button>";
-                            }
-                        }
-                        echo "</td>";
-                        echo "</tr>";
-                    }
-                }
-                else{
-                    echo "<tr><td class='align-middle' colspan='7'><span class='text-danger mb-3'>No data found.</span></td></tr>";
-                }
-                echo "</tbody></table></div></div>";
-            ?>
+            <div class="table-responsive">
+                <table>
+                    <thead><tr>
+                        <th scope='col' class='align-middle'><a href="#" class="link-dark">Team Summary</a></th>
+                        <th scope='col' class='align-middle'>&nbsp;|&nbsp;</th>
+                        <th scope='col' class='align-middle'><a href="#" class="link-dark">Team Shooting Stats</a></th>
+                        <th scope='col' class='align-middle'>&nbsp;|&nbsp;</th>
+                        <th scope='col' class='align-middle'><a href="#" class="link-dark">Team Shooting Stats (detailed)</a></th>
+                        <th scope='col' class='align-middle'>&nbsp;|&nbsp;</th> 
+                        <th scope='col' class='align-middle'><a href="#" class="link-dark">Team Basic Stats</a></th>
+                    </tr></thead>
+                </table>
+            </div>
         </div>
 
+        <div class="container mt4">
+                
+        </div>
         <!-- Bootstrap JavaScript Bundle with Popper -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
